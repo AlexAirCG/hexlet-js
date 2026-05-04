05e4feab874d400ffab5e027ca85897da1a7f3a1
AlexAirCG <alexairggg@yandex.ru>
2026-05-04 15:04:30 +0300

    added tests/set.test.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,46 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
-import stylistic from "@stylistic/eslint-plugin";
+import prettierConfig from "eslint-config-prettier"; // Только этот конфиг!
 
-export default defineConfig([
-  stylistic.configs.recommended,
+export default [
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    ignores: ["**/*.html"],
+  },
+  js.configs.recommended, // Базовые правила логики (ошибки, неиспользуемые переменные)
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+      },
+    },
     rules: {
-      "@stylistic/quotes": ["error", "double"], // Разрешаем двойные кавычки
-      "@stylistic/semi": ["error", "always"], // Требуем точки с запятой
+      "no-console": "off",
+      "no-unused-vars": "warn",
+      // Здесь ТОЛЬКО правила логики. Никаких кавычек и точек с запятой!
     },
   },
-]);
+  prettierConfig, // ОБЯЗАТЕЛЬНО ПОСЛЕДНИМ. Он отключит все правила ESLint, которые могут мешать Prettier.
+];
+// =====================================================
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+// import stylistic from "@stylistic/eslint-plugin";
+
+// export default defineConfig([
+//   stylistic.configs.recommended,
+//   {
+//     files: ["**/*.{js,mjs,cjs}"],
+//     plugins: { js },
+//     extends: ["js/recommended"],
+//     languageOptions: { globals: globals.node },
+//     rules: {
+//       "@stylistic/quotes": ["error", "double"], // Разрешаем двойные кавычки
+//       "@stylistic/semi": ["error", "always"], // Требуем точки с запятой
+//     },
+//   },
+// ]);
---
+++ src/set.js
@@ -0,0 +1,9 @@
+const set = (obj, key, value) => {
+  if (key in obj) return obj
+
+  obj[key] = value
+
+  return obj
+}
+
+export default set
